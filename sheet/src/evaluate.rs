acfd50d5d1d9b79fc569171f76f7b96116563b85
ShogunYash <shindekaryash16@gmail.com>
2025-04-06 19:20:12 +0530

    sleep added

--- sheet/src/evaluate.rs
+++ sheet/src/evaluate.rs
@@ -6,6 +6,43 @@ pub fn get_key(row: i16, col: i16, cols: i16) -> i32 {
     ((row as i32 )* (cols as i32) + (col as i32)) as i32
 }
 
+pub fn handle_sleep(
+    sheet: &mut Spreadsheet,
+    cell: &mut Cell,
+    row: i16,
+    col: i16,
+    expr: &str,
+    sleep_time: &mut f64,
+) -> CommandStatus{
+    let cell_ref = cell.parse_cell_reference(sheet, expr);
+    if cell_ref == Ok(_) {
+        let (row, col) = cell_ref.unwrap();
+        let parent_cell = sheet.get_cell(row, col).unwrap();
+        cell.formula = 102;    // Custom formula code for sleep
+        cell.parent1 = get_key(row, col, sheet.cols);
+        cell.parent2 = -1;    // No second parent for sleep
+        cell.value = parent_cell.value.clone();
+        if let CellValue::Integer(value) = parent_cell.value {
+            *sleep_time += value as f64;
+        }
+        else{
+            cell.value = CellValue::Error;
+        }
+    }
+    else if Ok(val) == expr.parse::<i32>() {
+        cell.value = CellValue::Integer(val);
+        *sleep_time += val;
+        cell.formula = -1;
+        cell.parent1 = -1;
+        cell.parent2 = -1;
+    }
+    else{
+        return CmdUnrecognized;
+    }
+
+    CmdOk
+}
+
 pub fn evaluate_formula(
     sheet: &mut Spreadsheet,
     cell: &mut Cell,
@@ -53,8 +90,8 @@ pub fn evaluate_formula(
             9 // is_stdev case
         };
 
-        cell.cell1 = get_key(range.start_row, range.start_col, sheet.cols);
-        cell.cell2 = get_key(range.end_row, range.end_col, sheet.cols);
+        cell.parent1 = get_key(range.start_row, range.start_col, sheet.cols);
+        cell.parent2 = get_key(range.end_row, range.end_col, sheet.cols);
 
 
         // Evaluate the function.
@@ -74,7 +111,15 @@ pub fn evaluate_formula(
         return CommandStatus::CmdOk;
     }
 
-    CommandStatus::CmdUnrecognized
+    else if expr.starts_with("SLEEP(") {
+        if !expr.ends_with(')') {
+            return CommandStatus::CmdUnrecognized;
+        }
+
+        let sleep_str = &expr[6..expr_len - 1];
+        handle_sleep(sheet, cell, row, col, sleep_str, sleep_time);
+    }
+    CommandStatus::CmdOk
 }
 
 
