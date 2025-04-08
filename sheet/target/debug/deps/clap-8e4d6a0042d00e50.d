407506d922264be727ebfd8ad193c8b2e7a28cd4
KARTHIK MANIKANDAN RAVIKUMAR <149307601+BEAST-33@users.noreply.github.com>
2025-04-08 15:18:52 +0530

    maintaining the same code

--- sheet/target/debug/deps/clap-8e4d6a0042d00e50.d
+++
@@ -1,7 +0,0 @@
-D:\Repos\Ferro_spreadsheet\sheet\target\debug\deps\libclap-8e4d6a0042d00e50.rmeta: C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\clap-4.5.34\src\lib.rs C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\clap-4.5.34\src\../examples/demo.rs C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\clap-4.5.34\src\../examples/demo.md
-
-D:\Repos\Ferro_spreadsheet\sheet\target\debug\deps\clap-8e4d6a0042d00e50.d: C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\clap-4.5.34\src\lib.rs C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\clap-4.5.34\src\../examples/demo.rs C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\clap-4.5.34\src\../examples/demo.md
-
-C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\clap-4.5.34\src\lib.rs:
-C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\clap-4.5.34\src\../examples/demo.rs:
-C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\clap-4.5.34\src\../examples/demo.md:
--- sheet/target/debug/deps/windows_targets-d4eaade192ddf4c7.d
+++
@@ -1,5 +0,0 @@
-D:\Repos\Ferro_spreadsheet\sheet\target\debug\deps\libwindows_targets-d4eaade192ddf4c7.rmeta: C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.52.6\src\lib.rs
-
-D:\Repos\Ferro_spreadsheet\sheet\target\debug\deps\windows_targets-d4eaade192ddf4c7.d: C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.52.6\src\lib.rs
-
-C:\Users\shind\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.52.6\src\lib.rs:
