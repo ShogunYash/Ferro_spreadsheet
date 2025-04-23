c5a7f32325ef8974c222a4b2c6f76a7a0284600c
Yash Sunil Shindekar <shindekaryash16@gmail.com>
2025-04-23 08:39:12 +0530

    Process command added

---
+++ src/process_command.rs
@@ -0,0 +1,25 @@
+use crate::evaluator::handle_command;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+use crate::spreadsheet::{CommandStatus, Spreadsheet};
+
+pub fn process_command(sheet: &mut Spreadsheet, command: &str, last_time: &mut f64) -> CommandStatus {
+    // Process the command and measure execution time
+    let mut sleep_time = 0.0; // Initialize sleep_time to 0.0
+    let command_time ;
+    // Pass by reference instead of cloning
+    let start = Instant::now();
+    let status = handle_command(sheet, command, &mut sleep_time);
+    command_time = start.elapsed().as_secs_f64();
+
+    if sleep_time <= command_time {
+        sleep_time = 0.0;
+    } else {
+        sleep_time -= command_time;
+    }
+    *last_time = command_time + sleep_time;
+    if sleep_time > 0.0 {
+        sleep(Duration::from_secs_f64(sleep_time));
+    }
+    status
+}
